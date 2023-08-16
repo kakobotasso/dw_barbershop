a5b5e7a5eea70d25ae95d715ae165d5149301af6
Kako Botasso <kakodev@gmail.com>
2023-08-16 11:30:17 -0300

    Create provider to get User information

--- lib/src/repositories/user/user_repository.dart
+++ lib/src/repositories/user/user_repository.dart
@@ -1,6 +1,11 @@
 import 'package:dw_barbershop/src/core/exceptions/auth_exception.dart';
 import 'package:dw_barbershop/src/core/fp/either.dart';
 
+import '../../core/exceptions/repository_exception.dart';
+import '../../model/user_model.dart';
+
 abstract interface class UserRepository {
   Future<Either<AuthException, String>> login(String email, String password);
+
+  Future<Either<RepositoryException, UserModel>> me();
 }
