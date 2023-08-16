b329f9fc9c5b07e6f28965e87d97ccb9712232d7
Kako Botasso <kakodev@gmail.com>
2023-08-15 23:59:42 -0300

    Make login page communicate with service

---
+++ lib/src/features/auth/login/login_vm.dart
@@ -0,0 +1,37 @@
+import 'package:asyncstate/class/async_loader_handler.dart';
+import 'package:dw_barbershop/src/core/exceptions/service_exception.dart';
+import 'package:dw_barbershop/src/core/fp/either.dart';
+import 'package:dw_barbershop/src/core/providers/application_providers.dart';
+import 'package:dw_barbershop/src/features/auth/login/login_state.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'login_vm.g.dart';
+
+@riverpod
+class LoginVm extends _$LoginVm {
+  @override
+  LoginState build() => LoginState.initial();
+
+  Future<void> login(String email, String password) async {
+    final loaderHandler = AsyncLoaderHandler()..start();
+
+    final loginService = ref.watch(userLoginServiceProvider);
+
+    final result = await loginService.execute(email, password);
+
+    switch(result) {
+      case Success():
+        // buscar dados do usuário logado
+        // fazer análise para qual o tipo do login
+        break;
+      case Failure(exception: ServiceException(:final message)):
+        state = state.copyWith(
+          status: LoginStateStatus.error,
+          errorMessage: () => message
+        );
+      case _:
+        break;
+    }
+    loaderHandler.close();
+  }
+}
---
+++ lib/src/features/auth/login/login_vm.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'login_vm.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$loginVmHash() => r'5e1c8b9b12f5dcd770a5b6e7431f66252779c6d5';
+
+/// See also [LoginVm].
+@ProviderFor(LoginVm)
+final loginVmProvider =
+    AutoDisposeNotifierProvider<LoginVm, LoginState>.internal(
+  LoginVm.new,
+  name: r'loginVmProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$loginVmHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$LoginVm = AutoDisposeNotifier<LoginState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
