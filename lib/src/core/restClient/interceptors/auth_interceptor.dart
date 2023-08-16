f710116a502c4da357e3f81bfe764ee33483f7ee
Kako Botasso <kakodev@gmail.com>
2023-08-16 16:11:22 -0300

    Add onError to AuthInterceptor

--- lib/src/core/restClient/interceptors/auth_interceptor.dart
+++ lib/src/core/restClient/interceptors/auth_interceptor.dart
@@ -1,19 +1,23 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
+import 'package:dw_barbershop/src/core/ui/barbershop_nav_global_key.dart';
+import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../constants/local_storage_keys.dart';
 
 class AuthInterceptor extends Interceptor {
-
   @override
-  void onRequest(RequestOptions options, RequestInterceptorHandler handler) async {
+  void onRequest(
+      RequestOptions options, RequestInterceptorHandler handler) async {
     final RequestOptions(:headers, :extra) = options;
 
-    const authHeaderKey =  'Authorization';
+    const authHeaderKey = 'Authorization';
 
     headers.remove(authHeaderKey);
 
-    if(extra case {'DIO_AUTH_KEY': true}) {
+    if (extra case {'DIO_AUTH_KEY': true}) {
       final sp = await SharedPreferences.getInstance();
       headers.addAll({
         authHeaderKey: 'Bearer ${sp.getString(LocalStorageKeys.accessToken)}'
@@ -22,4 +26,16 @@ class AuthInterceptor extends Interceptor {
     handler.next(options);
   }
 
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) {
+    final DioException(requestOptions: RequestOptions(:extra), :response) = err;
+
+    if (extra case {'DIO_AUTH_KEY': true}) {
+      if (response != null && response.statusCode == HttpStatus.forbidden) {
+        Navigator.of(BarbershopNavGlobalKey.instance.navKey.currentContext!)
+            .pushNamedAndRemoveUntil('/auth/login', (route) => false);
+      }
+    }
+    handler.reject(err);
+  }
 }
