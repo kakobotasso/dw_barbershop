71fd3297dc846f7db190b10a5405db5540eb9c4e
Kako Botasso <kakodev@gmail.com>
2023-08-17 01:23:40 -0300

    Check if user already logged in and show him the correct page

---
+++ lib/src/features/splash/splash_vm.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'splash_vm.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$splashVmHash() => r'1a8e4d4a166f6b3d90ce9d75b88e15b178e5d57b';
+
+/// See also [SplashVm].
+@ProviderFor(SplashVm)
+final splashVmProvider =
+    AutoDisposeAsyncNotifierProvider<SplashVm, SplashState>.internal(
+  SplashVm.new,
+  name: r'splashVmProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$splashVmHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$SplashVm = AutoDisposeAsyncNotifier<SplashState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
