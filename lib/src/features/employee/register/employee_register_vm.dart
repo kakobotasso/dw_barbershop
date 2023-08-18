fb2a8775115d48e6d0a7aaf48a9b86e5706d6aa8
Kako Botasso <kakodev@gmail.com>
2023-08-17 21:52:32 -0300

    Integrate EmployeeRegisterPage with backend

---
+++ lib/src/features/employee/register/employee_register_vm.dart
@@ -0,0 +1,88 @@
+import 'package:asyncstate/class/async_loader_handler.dart';
+import 'package:dw_barbershop/src/core/exceptions/repository_exception.dart';
+import 'package:dw_barbershop/src/core/providers/application_providers.dart';
+import 'package:dw_barbershop/src/features/employee/register/employee_register_state.dart';
+import 'package:dw_barbershop/src/model/barbershop_model.dart';
+import 'package:dw_barbershop/src/repositories/user/user_repository.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../../../core/fp/either.dart';
+import '../../../core/fp/nil.dart';
+
+part 'employee_register_vm.g.dart';
+
+@riverpod
+class EmployeeRegisterVm extends _$EmployeeRegisterVm {
+  @override
+  EmployeeRegisterState build() => EmployeeRegisterState.initial();
+
+  void setRegisterAdm(bool isRegisterAdm) {
+    state = state.copyWith(registerADM: isRegisterAdm);
+  }
+
+  void addOrRemoveWorkDays(String weekDay) {
+    final EmployeeRegisterState(:workDays) = state;
+
+    if (workDays.contains(weekDay)) {
+      workDays.remove(weekDay);
+    } else {
+      workDays.add(weekDay);
+    }
+
+    state = state.copyWith(workDays: workDays);
+  }
+
+  void addOrRemoveWorkHours(int hour) {
+    final EmployeeRegisterState(:workHours) = state;
+
+    if (workHours.contains(hour)) {
+      workHours.remove(hour);
+    } else {
+      workHours.add(hour);
+    }
+
+    state = state.copyWith(workHours: workHours);
+  }
+
+  Future<void> register({String? name, String? email, String? password}) async {
+    final EmployeeRegisterState(:registerADM, :workDays, :workHours) = state;
+    final asyncLoaderHandler = AsyncLoaderHandler()..start();
+
+    final UserRepository(:registerAdmAsEmployee, :registerEmployee) =
+        ref.read(userRepositoryProvider);
+
+    final Either<RepositoryException, Nil> resultRegister;
+
+    if (registerADM) {
+      final dto = (
+        workDays: workDays,
+        workHours: workHours,
+      );
+      resultRegister = await registerAdmAsEmployee(dto);
+    } else {
+      final BarbershopModel(:id) =
+          await ref.watch(getMyBarbershopProvider.future);
+      final dto = (
+        barbershopId: id,
+        name: name!,
+        email: email!,
+        password: password!,
+        workDays: workDays,
+        workHours: workHours,
+      );
+
+      resultRegister = await registerEmployee(dto);
+    }
+
+    switch (resultRegister) {
+      case Success():
+        state = state.copyWith(status: EmployeeRegisterStateStatus.success);
+        break;
+      case Failure():
+        state = state.copyWith(status: EmployeeRegisterStateStatus.error);
+        break;
+    }
+
+    asyncLoaderHandler.close();
+  }
+}
--- lib/src/repositories/user/user_repository.dart
+++ lib/src/repositories/user/user_repository.dart
@@ -19,4 +19,18 @@ abstract interface class UserRepository {
   );
 
   Future<Either<RepositoryException, List<UserModel>>> getEmployees(int barbershopId);
+
+  Future<Either<RepositoryException, Nil>> registerAdmAsEmployee(({
+    List<String> workDays,
+    List<int> workHours,
+  }) userModel);
+
+  Future<Either<RepositoryException, Nil>> registerEmployee(({
+    int barbershopId,
+    String name,
+    String email,
+    String password,
+    List<String> workDays,
+    List<int> workHours,
+  }) userModel);
 }
